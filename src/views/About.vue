33aa3482e85f98e33e118ee0cd386e80c7e49f62
KINGMJ <328047478@qq.com>
2020-09-28 09:07:22 +0800

    新增demo页面

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <demo-preview msg="Vue3 和 TypeScript 的尝试性 demo" />
   </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import DemoPreview from "@/components/DemoPreview.vue";
+
+@Options({
+  components: {
+    DemoPreview,
+  },
+})
+export default class About extends Vue {}
+</script>
