139ecd46a1f3704ff2758f6edb4361b942730a8b
KINGMJ <328047478@qq.com>
2021-02-02 15:06:10 +0800

    新增v-on.native示例

---
+++ src/components/demo14/Component1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <span>这是一个span</span>
+    <button @click="$emit('close')">确定</button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  inheritAttrs: false,
+  emits: ['close']
+})
+</script>
---
+++ src/views/Demo14.vue
@@ -0,0 +1,21 @@
+<template>
+  <component-1 v-on:close="handleComponentEvent" v-on:click="handleNativeClickEvent" />
+</template>
+
+<script lang="ts">
+import Component1 from '@/components/demo14/Component1.vue'
+import { defineComponent } from 'vue'
+export default defineComponent({
+  methods: {
+    handleComponentEvent() {
+      console.log('处理的是一个组件的事件')
+    },
+    handleNativeClickEvent() {
+      console.log('处理的是一个原生事件')
+    }
+  },
+  components: {
+    Component1
+  }
+})
+</script>
