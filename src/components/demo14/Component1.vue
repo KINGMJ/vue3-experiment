d89431c945645d1dc449c655b9303114e6e70eda
KINGMJ <328047478@qq.com>
2021-02-07 11:08:43 +0800

    添加注释

--- src/components/demo14/Component1.vue
+++ src/components/demo14/Component1.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
@@ -13,3 +13,8 @@ export default defineComponent({
   emits: ['close']
 })
 </script>
+
+<!--
+1. 在 vue3 中，组件的事件需要在子组件中通过 emits 来指定；如果未指定 emits ，会被当做 DOM 的原生事件添加到子组件的根元素上
+2. 如果子组件指定了 inheritAttrs: false , 不会添加到子组件的根元素上
+-->
