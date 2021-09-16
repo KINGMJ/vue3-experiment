45701e9122a209448d65e84d354999b01aed3bdb
KINGMJ <328047478@qq.com>
2021-09-16 15:01:53 +0800

    key 和 id 的传递

---
+++ src/components/demo22/Component1.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ key }}
+    {{ id }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Component1',
+  props: {
+    key: String,
+    id: String
+  },
+  data() {
+    return {
+      name: 'jack'
+    }
+  }
+}
+</script>
---
+++ src/views/Demo22.vue
@@ -0,0 +1,11 @@
+<template>
+  <component-1 key="10" id="2" />
+</template>
+
+<script lang="ts">
+import Component1 from '@/components/demo22/Component1.vue'
+import { defineComponent } from 'vue'
+export default defineComponent({
+  components: { Component1 }
+})
+</script>
